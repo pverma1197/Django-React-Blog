1d95c9538dd5fe2d10dc779ba8d76c9eb1261033
Pooja Verma <vermapooja1197@gmail.com>
2021-12-07 12:52:12 +0530

    Update settings.py

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -145,9 +145,9 @@ SIMPLE_JWT = {
 
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mytrialacaunt@gmail.com'
-EMAIL_HOST_PASSWORD = 'Trial@123'
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'mytrialacaunt@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Trial@123'
+# EMAIL_USE_TLS = True
